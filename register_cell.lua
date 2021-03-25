5da9e736600c3616db8329c15ecde2fdcd29b406
Patrick Kurth <p.kurth@posteo.de>
2021-03-25 19:46:16 +0100

    Updated register

--- register_cell.lua
+++ register_cell.lua
@@ -2,8 +2,10 @@
     INV   INV    OAI_221     FF_OUT  
    OAI_21 INV    OAI_221     FF_BUF
           NAND   OAI_221     FF_IN
-]] --
-function parameters() pcell.reference_cell("logic/base") end
+--]]
+function parameters() 
+    pcell.reference_cell("logic/base")
+end
 
 function layout(gate, _P)
     local bp = pcell.get_parameters("logic/base")
@@ -13,37 +15,39 @@ function layout(gate, _P)
     -- general settings
     pcell.push_overwrites("logic/base", {leftdummies = 0, rightdummies = 0})
 
-    -- or_and_inv_221 gate & ff_in 
-    local orandinv221_in = pcell.create_layout("or_and_inv_221"):move_anchor("right")
-    gate:merge_into(orandinv221_in)
+    -- ff_in row 
+    local orandinv221_in = pcell.create_layout("or_and_inv_221"):move_anchor("top")
+    gate:merge_into_update_alignmentbox(orandinv221_in)
 
-    local ff_in = pcell.create_layout("logic/dff"):move_anchor("left")
-    gate:merge_into(ff_in)
+    local ff_in = pcell.create_layout("logic/dff"):move_anchor("left", orandinv221_in:get_anchor( "right"))
+    gate:merge_into_update_alignmentbox(ff_in)
 
-    -- or_and_inv_221 gate & ff_buf, location is above ff_in, needs to be flipped in y axis
+    -- ff_buf row
     local orandinv221_buf = pcell.create_layout("or_and_inv_221")
     orandinv221_buf:flipy()
-    orandinv221_buf:move_anchor("VDD", orandinv221_in:get_anchor("VDD"))
-    gate:merge_into(orandinv221_buf)
+    orandinv221_buf:move_anchor("bottom")
+    gate:merge_into_update_alignmentbox(orandinv221_buf)
 
     local ff_buf = pcell.create_layout("logic/dff"):move_anchor("left", orandinv221_buf:get_anchor( "right"))
     ff_buf:flipy()
-    gate:merge_into(ff_buf)
-    local inv_buf = pcell.create_layout("logic/not_gate"):move_anchor("right", orandinv221_buf:get_anchor( "left"))
-    inv_buf:flipy()
-    gate:merge_into(inv_buf)
+    gate:merge_into_update_alignmentbox(ff_buf)
 
-    local orandinv21 = pcell.create_layout("or_and_inv_21"):move_anchor( "right", orandinv221_buf:get_anchor("left"))
+    local inv_buf = pcell.create_layout("logic/not_gate"):move_anchor("right", orandinv221_buf:get_anchor("left"))
+    inv_buf:flipy()
+    gate:merge_into_update_alignmentbox(inv_buf)
+
+    local orandinv21 = pcell.create_layout("or_and_inv_21"):move_anchor("right", orandinv221_buf:get_anchor( "left"))
     orandinv21:flipy()
-    gate:merge_into(orandinv21)
+    gate:merge_into_update_alignmentbox(orandinv21)
 
     -- or_and_inv_221 gate & ff_out, location is above ff_buf
-    local orandinv221_out = pcell.create_layout("or_and_inv_221"):move_anchor("VSS", orandinv221_buf:get_anchor( "VSS"))
-    gate:merge_into(orandinv221_out)
+    local orandinv221_out = pcell.create_layout("or_and_inv_221"):move_anchor( "VSS", orandinv221_buf:get_anchor("VSS"))
+    gate:merge_into_update_alignmentbox(orandinv221_out)
 
     local ff_out = pcell.create_layout("logic/dff"):move_anchor("left", orandinv221_out:get_anchor( "right"))
-    gate:merge_into(ff_out)
+    gate:merge_into_update_alignmentbox(ff_out)
 
     local inv_out = pcell.create_layout("logic/not_gate"):move_anchor("right", orandinv221_out:get_anchor( "left"))
-    gate:merge_into(inv_out)
+    gate:merge_into_update_alignmentbox(inv_out)
 end
+
