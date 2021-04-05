9737409968bc6c305912d29cd139fe960e6cbcc3
Philipp Nickel <philipp.nickel@campus.tu-berlin.de>
2021-04-05 18:08:31 +0200

    added nor3

---
+++ nor3.lua
@@ -0,0 +1,49 @@
+--[[  
+A1 ---- OR
+        OR
+A2 ---- OR ---- NOR
+                NOR ---- Z
+A3 ------------ NOR
+]] function parameters() pcell.reference_cell("logic/base") end
+
+function layout(gate, _P)
+    local bp = pcell.get_parameters("logic/base");
+
+    local xpitch = bp.gspace + bp.glength
+
+    -- general settings
+    pcell.push_overwrites("logic/base", {leftdummies = 0, rightdummies = 1})
+
+    local orgate = pcell.create_layout("logic/or_gate"):move_anchor("right")
+    gate:merge_into_update_alignmentbox(orgate)
+
+    local norgate = pcell.create_layout("logic/nor_gate"):move_anchor("left",
+                                                                      orgate:get_anchor(
+                                                                          "right"))
+    gate:merge_into_update_alignmentbox(norgate)
+
+    -- draw connections
+    gate:merge_into(geometry.path_xy(generics.metal(2), {
+        orgate:get_anchor("Z"), norgate:get_anchor("A")
+    }, bp.sdwidth))
+    gate:merge_into(geometry.rectangle(generics.via(1, 2), bp.sdwidth,
+                                       bp.sdwidth):translate(
+                        orgate:get_anchor("Z")))
+    gate:merge_into(geometry.rectangle(generics.via(1, 2), bp.sdwidth,
+                                       bp.sdwidth):translate(
+                        norgate:get_anchor("A")))
+
+
+    gate:add_port("A1", generics.metal(1), orgate:get_anchor("A"))
+    gate:add_port("A2", generics.metal(1), orgate:get_anchor("B"))
+    gate:add_port("A3", generics.metal(1), norgate:get_anchor("B"))
+    gate:add_port("Z", generics.metal(1), norgate:get_anchor("Z"))
+    gate:add_port("VDD", generics.metal(1), point.create(0, bp.separation / 2 +
+                                                             bp.pwidth +
+                                                             bp.powerspace +
+                                                             bp.powerwidth / 2))
+    gate:add_port("VSS", generics.metal(1), point.create(0, -bp.separation / 2 -
+                                                             bp.nwidth -
+                                                             bp.powerspace -
+                                                             bp.powerwidth / 2))
+end
