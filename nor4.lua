f1ae7217e5bd884bba9ba4b8435aadbf72520831
Philipp Nickel <philipp.nickel@campus.tu-berlin.de>
2021-04-05 14:29:58 +0200

    finished nor4

---
+++ nor4.lua
@@ -0,0 +1,60 @@
+--[[
+A1 ---- OR
+        OR
+A2 ---- OR ---- NOR 
+                NOR ---- Z
+A3 ---- OR ---- NOR
+        OR
+A4 ---- OR
+]] function parameters() pcell.reference_cell("logic/base") end
+
+function layout(gate, _P)
+    local bp = pcell.get_parameters("logic/base");
+
+    local xpitch = bp.gspace + bp.glength
+
+    -- general settings
+    pcell.push_overwrites("logic/base", {leftdummies = 0, rightdummies = 0})
+
+    -- place cells
+    local orgate_a = pcell.create_layout("logic/or_gate"):move_anchor("right")
+    gate:merge_into_update_alignmentbox(orgate_a)
+
+    local orgate_b = pcell.create_layout("logic/or_gate"):move_anchor("left",
+                                                                        orgate_a:get_anchor(
+                                                                            "right"))
+    gate:merge_into_update_alignmentbox(orgate_b)
+
+    local norgate = pcell.create_layout("logic/nor_gate"):move_anchor("left",
+                                                                      orgate_b:get_anchor(
+                                                                          "right"))
+    gate:merge_into_update_alignmentbox(norgate)
+
+    -- draw connections
+    gate:merge_into(geometry.path_xy(generics.metal(2), {
+        orgate_a:get_anchor("Z"), norgate:get_anchor("A")
+    }, bp.sdwidth))
+    gate:merge_into(geometry.rectangle(generics.via(1, 2), bp.sdwidth,
+                                       bp.sdwidth):translate(
+                        orgate_a:get_anchor("Z")))
+    gate:merge_into(geometry.rectangle(generics.via(1, 2), bp.sdwidth,
+                                       bp.sdwidth):translate(
+                        norgate:get_anchor("B")))
+
+    gate:merge_into(geometry.path_xy(generics.metal(2), {
+        orgate_b:get_anchor("Z"), norgate:get_anchor("B")
+    }, bp.sdwidth))
+    gate:merge_into(geometry.rectangle(generics.via(1, 2), bp.sdwidth,
+                                       bp.sdwidth):translate(
+                        orgate_b:get_anchor("Z")))
+    gate:merge_into(geometry.rectangle(generics.via(1, 2), bp.sdwidth,
+                                       bp.sdwidth):translate(
+                        norgate:get_anchor("A")))
+
+    -- draw ports
+    gate:add_port("A1", generics.metal(1), orgate_a:get_anchor("A"))
+    gate:add_port("A2", generics.metal(1), orgate_a:get_anchor("B"))
+    gate:add_port("A3", generics.metal(1), orgate_b:get_anchor("A"))
+    gate:add_port("A4", generics.metal(1), orgate_b:get_anchor("B"))
+    gate:add_port("Z", generics.metal(1), norgate:get_anchor("Z"))
+end
