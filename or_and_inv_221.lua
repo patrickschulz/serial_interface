35f08dbce6b8f34df9ffe91623fb6b0bb6aacc63
Philipp Nickel <philipp.nickel@campus.tu-berlin.de>
2021-04-23 22:54:38 +0200

    fixed merge

--- or_and_inv_221.lua
+++ or_and_inv_221.lua
@@ -14,7 +14,6 @@ function parameters() pcell.reference_cell("logic/base") end
 function layout(gate, _P)
     local bp = pcell.get_parameters("logic/base");
 
-<<<<<<< HEAD
     local xpitch = bp.gspace + bp.glength
 
     -- or gate c
@@ -39,43 +38,14 @@ function layout(gate, _P)
     pcell.push_overwrites("logic/base", {leftdummies = 1})
     local nandgate = pcell.create_layout("logic/nand_gate"):move_anchor("left", orgate_b:get_anchor( "right"))
     gate:merge_into_update_alignmentbox(nandgate)
-=======
-    -- or gate c
-    pcell.push_overwrites("logic/base", { rightdummies = 1})
-    local orgate_c = pcell.create_layout("logic/or_gate"):move_anchor("right")
-    gate:merge_into(orgate_c)
-    pcell.pop_overwrites("logic/base")
-
-    -- and gate
-    pcell.push_overwrites("logic/base", { leftdummies = 1, rightdummies = 1})
-    local andgate = pcell.create_layout("logic/and_gate"):move_anchor("left", orgate_c:get_anchor( "right"))
-    gate:merge_into(andgate)
-    pcell.pop_overwrites("logic/base")
-
-    -- or gate b
-    pcell.push_overwrites("logic/base", { leftdummies = 1, rightdummies = 1})
-    local orgate_b = pcell.create_layout("logic/or_gate"):move_anchor("left", andgate:get_anchor( "right"))
-    gate:merge_into(orgate_b)
-    pcell.pop_overwrites("logic/base")
-
-    -- nand gate
-    pcell.push_overwrites("logic/base", { leftdummies = 1 })
-    local nandgate = pcell.create_layout("logic/nand_gate"):move_anchor("left", orgate_b:get_anchor( "right"))
-    gate:merge_into(nandgate)
->>>>>>> 6654b638eb623f73641a4b81aecbf3c15510c65e
     pcell.pop_overwrites("logic/base")
 
     -- draw connections
     gate:merge_into(geometry.path_yx(generics.metal(1), {
         orgate_c:get_anchor("Z"), andgate:get_anchor("B")
     }, bp.sdwidth))
-<<<<<<< HEAD
     gate:merge_into(geometry.path_xy(generics.metal(1), {
         orgate_b:get_anchor("Z"), nandgate:get_anchor("B")
-=======
-    gate:merge_into(geometry.path_yx(generics.metal(1), {
-        orgate_b:get_anchor("Z"), nandgate:get_anchor("A")
->>>>>>> 6654b638eb623f73641a4b81aecbf3c15510c65e
     }, bp.sdwidth))
     gate:merge_into(geometry.path_yx(generics.metal(2), {
         andgate:get_anchor("Z"), nandgate:get_anchor("A")
